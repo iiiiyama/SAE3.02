283bc7a26871412919fe77cd6fcca52638a3b8aa
SAE3.02 <bptstantony@gmail.com>
2022-11-24 09:20:37 +0100

    début interface graphique

---
+++ GUI.py
@@ -0,0 +1,14 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit, QComboBox, QPushButton, QGridLayout
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        widget = QWidget()
+        self.setCentralWidget(widget)
+
+        grid = QGridLayout()
+
+        widget.setLayout(grid)
