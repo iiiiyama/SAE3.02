d84e7714cc4249e3d79313e95dc2c53fb49d5aaf
Baptiste Antony <bptstantony@gmail.com>
2022-11-28 11:11:26 +0100

    serveur asynchrone opérationel dans cet état

--- client.py
+++ client.py
@@ -6,27 +6,28 @@ def client():
 
     host = 'localhost'
     port = 5000
-    client_socket = socket.socket()
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     client_socket.connect((host, port))
     while True:
         msg = input("->")
         client_socket.send(msg.encode())
-        data = client_socket.recv(1024).decode()
-        print('received from server: ' + data)
-        if data.lower() == "bye":
-            client_socket.close()
+        data = client_socket.recv(1024)
+        print('received from server: ' + (data.decode()))
+        #if data.lower() == "bye":
+        #    client_socket.close()
 
-        if msg == 'os' or 'OS':
-            if platform.system() == 'Windows':
-                print(platform.system())
-                if msg == 'ip' or 'ipconfig':
-                    print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0])
+        #if msg == 'os' or msg == 'OS':
+        #    if platform.system() == 'Windows':
+        #        print(platform.system())
+        #    if msg == 'ip' or msg == 'ipconfig':
+        #        print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0])
 
-            elif platform.system() == 'Linux':
-                print(platform.system())
-                if msg == 'ip a' or 'ip address':
-                    print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0])
+        #    elif platform.system() == 'Linux':
+        #        print(platform.system())
+        #        if msg == 'ip a' or 'ip address':
+        #            print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0])
+    client_socket.close()
 
 
 if __name__ == '__main__':
--- serveur.py
+++ serveur.py
@@ -1,25 +1,41 @@
 import socket
+import threading
+from _thread import *
+
+no_print = threading.Lock()
+
+
+def thread(conn_client):
+    while True:
+        data = conn_client.recv(1024)
+        if not data:
+            print("bye")
+            no_print.release()
+            break
+        conn_client.send(data)
+    conn_client.close()
 
 
 def serveur():
     host = ""
     port = 5000
-    server_socket = socket.socket()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(1)
-    conn_client, client_address = server_socket.accept()
+    server_socket.listen(5)
+
     while True:
-        data = conn_client.recv(1024).decode()
-        print(f"from connected user {host}:" + str(data))
-        if data == 'bye':
-            conn_client.send('bye'.encode())
-            conn_client.close()
-            server_socket.close()
-            break
-        data = input("->")
-        conn_client.send(data.encode())
-    #while False:
-        #print("en attente d'un client ...")
+        conn_client, client_address = server_socket.accept()
+        no_print.acquire()
+        print("connected to :", client_address[0], "on the port", client_address[1])
+        start_new_thread(thread, (conn_client,))
+    server_socket.close()
+    # data = ''
+    # if data == 'bye':
+    #    conn_client.send('bye'.encode())
+    #    conn_client.close()
+    #
+    #    break
+    # conn_client.send(data.encode())
 
 
 if __name__ == '__main__':
